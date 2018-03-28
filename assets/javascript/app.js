db910e21dbcea89dfa5872c7ce48f780b36b2e82
Jessica Son <jessicason@airbears2-10-142-126-5.airbears2.1918.berkeley.edu>
2018-03-27 20:14:30 -0700

    for loop

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,34 @@
 var timeRemaining = 5
 var intervalId;
+var question = [
+    "What college did Gabriella Montez go to?",
+    "What is the name of Zac Efron's character in the movie?",
+    "QUESTION 3",
+    "QUESTION 4",
+    "QUESTION 5",
+];
+
+
+function question () {
+    $("#question").html(question1);
+}
+
+
+
+for (var i = 0; i < question.length; i++) {
+    var questionWeWant = question[i];
+    console.log("THIS IS WORKING!!!");
+} 
+
+
 
 function run() {
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
-    console.log("this works");
   }
-
+function stop() {
+    clearInterval(intervalId);s
+  }
 
 function decrement() {
     timeRemaining--;
@@ -18,8 +40,7 @@ function decrement() {
     }
   }
 
-  function stop() {
-    clearInterval(intervalId);
-  }
 
-  run();
+  
+run();
+  
