b024dfc43c2240124a2b0a5ffcca1789d693743a
Jessica Son <jessicason@Jessicas-MacBook-Pro.local>
2018-03-29 11:42:47 -0700

    about to start over

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,40 +7,96 @@ var question = [
     "QUESTION 4",
     "QUESTION 5",
 ];
+var answer1 = [
+    "Stanford",
+    "UC Berkeley",
+    "UC Irvine",
+    "Harvard",
+];
+var userAnswer;
+var correctAnswer1 = answer1[0];
+console.log(correctAnswer1);
+
+question1();
+
+function question1(){
+    $("#question").text("What college did Gabriella Montez go to?");
+    $("#answer1").empty();
+    for (var i=0; i< answer1.length; i++) {
+        buttons = $("<button>");
+        buttons.addClass("answer-btn");
+        buttons.attr("data-name"),answer1[i];
+        buttons.text(answer1[i]);
+        $("#answer1").append(buttons);
+    }
+   
+    $("#answer1").click(function(event) {
+        userAnswer = event.target.id;
+        console.log(userAnswer);
+        if (userAnswer === correctAnswer1) {
+          console.log("You win!");
+        }
+        else {
+          
+        }
+      });
+
+}   
 
 
-function question () {
-    $("#question").html(question1);
+
+
+/*
+      $("#answer1").on("click", function(event) { 
+        $("#correctGif").html("<img src='assets/images/correctGif.gif' width='200px'/>");
+    })
+
+
+    $(".answer1[0]").on("click", function() {
+        $("#correctGif").html("<img src='assets/images/correctGif.gif' width='200px'/>");
+        console.log("this works");
+    });
+
+
+
+
+    $("#answer1").on("click", ".answer-button", function() {
+        var b = $("<div>");
+        b.addclass("giphy");
+        b.text($(this).attr("data-name"));
+        main.find("display").append(b);
+        console.log('HELLO!!!!!!!');
+    });
+    
+    
+
 }
 
 
-
-for (var i = 0; i < question.length; i++) {
-    var questionWeWant = question[i];
-    console.log("THIS IS WORKING!!!");
-} 
 
 
 
 function run() {
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
+    $(".question").text(question);
+    console.log(question);
   }
+
 function stop() {
-    clearInterval(intervalId);s
+    clearInterval(intervalId);
   }
 
 function decrement() {
     timeRemaining--;
-    console.log(timeRemaining);
     $("#timeRemaining").html("Time Remaining: " + timeRemaining);
     if (timeRemaining === 0) {
       stop();
-      alert("Time Up!");
     }
   }
 
 
   
 run();
-  
+
+*/
